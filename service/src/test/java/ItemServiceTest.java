f06c8d375937cd51abdadb264dbf335aaccb0f0c
jedeb <jeroendebra@gmail.com>
2021-04-19 18:45:20 +0200

    added dumb test

--- service/src/test/java/ItemServiceTest.java
+++ service/src/test/java/ItemServiceTest.java
@@ -1,7 +1,13 @@
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ItemServiceTest {
+
+    @Test
+    public void test(){
+        assertThat(true).isTrue();
+    }
 
 }
